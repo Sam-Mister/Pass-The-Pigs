12771cba243d4ff5211cf1f82691dacf98a7cf76
Sam Mister <sm15883@bristol.ac.uk>
2021-03-06 14:13:31 +0000

    Added the basic logic needed to impliment the game.

--- pigs.js
+++ pigs.js
@@ -1,1 +1,30 @@
+let outcome;
+class pig{
+  constructor(x,y,state){
+    this.pos = createVector(x,y);
+    this.state = state;
+  }
+  roll = function(){
+    outcome = Math.random();
+    console.log(outcome);
+    if (outcome >= 0 && outcome <= 0.349){
+      this.state = "No Dot";
+    } else if (outcome > 0.349 && outcome <= 0.651){
+      this.state = "Dot";
+    } else if (outcome > 0.651 && outcome <= 0.875){
+      this.state = "Razorback"
+    } else if (outcome > 0.875 && outcome <= 0.963){
+      this.state = "Trotter"
+    } else if (outcome > 0.963 && outcome <= 0.993){
+      this.state = "Snouter"
+    } else {
+      this.state = "Leaning jowler"
+    }
 
+  }
+  show = function(){
+    circle(this.pos.x,this.pos.y,80)
+    text(this.state, this.pos.x, this.pos.y)
+    textAlign(CENTER, CENTER);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,1 +1,24 @@
+let cnv;
+let Pig1;
+let Pig2;
+let Button;
 
+function setup() {
+  cnv = createCanvas(window.innerWidth * (2/3), window.innerHeight)
+  Pig1 = new pig(-50,0,"Dot")
+  Pig2 = new pig(50,0,"Dot")
+
+  Button = createButton("Roll");
+  Button.position(width*(0.9),height/2)
+  Button.mousePressed(function (){
+    Pig1.roll();
+    Pig2.roll();
+  })
+}
+
+function draw() {
+  background(50);
+  translate(width/2,height/2)
+  Pig1.show();
+  Pig2.show();
+}
